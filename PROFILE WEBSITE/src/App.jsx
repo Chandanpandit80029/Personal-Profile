280b0013af3c06e7c0e97017c06e078c47583eb7
Chandan Pandit <125503076+Chandanpandit80029@users.noreply.github.com>
2026-06-03 12:41:44 +0530

    Change Router from BrowserRouter to HashRouter

--- PROFILE WEBSITE/src/App.jsx
+++ PROFILE WEBSITE/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './context/AuthContext';
